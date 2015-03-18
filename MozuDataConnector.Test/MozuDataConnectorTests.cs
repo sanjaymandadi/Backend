439356483814be78bfe3a3542ef3af085847fb98
MozuPS <Volusion123!>
2015-03-18 17:45:33 -0500

    Including all files

---
+++ MozuDataConnector.Test/MozuDataConnectorTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Autofac;
+using Mozu.Api;
+using Mozu.Api.Resources.Platform;
+using Mozu.Api.ToolKit;
+using Mozu.Api.ToolKit.Config;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MozuDataConnector.Test
+{
+
+
+    [TestClass]
+    public class MozuDataConnectorTests
+    {
+        private IApiContext _apiContext;
+        private IContainer _container;
+
+
+        [TestInitialize]
+        public void Init()
+        {
+            _container = new Bootstrapper().Bootstrap().Container;
+            var appSetting = _container.Resolve<IAppSetting>();
+            var tenantId = int.Parse(appSetting.Settings["TenantId"].ToString());
+            var siteId = int.Parse(appSetting.Settings["SiteId"].ToString());
+
+            _apiContext = new ApiContext(tenantId, siteId);
+        }
+
+        [TestMethod]
+        public void Should_Connect_To_Tenant()
+        {
+            var tenantResource = new TenantResource(_apiContext);
+        }
+    }
+}
