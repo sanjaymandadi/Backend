51618a39e80d3754f7449c71b9b6b2b3b473e2a6
mrsergio19 <sergiojtorres@outlook.com>
2015-03-19 04:03:12 -0500

    comm

---
+++ MozuDataConnector.Domain/Handlers/AttributeHandler.cs
@@ -0,0 +1,48 @@
+﻿using Mozu.Api;
+using Mozu.Api.Resources.Commerce.Catalog.Admin.Attributedefinition;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MozuDataConnector.Domain.Handlers
+{
+    public class AttributeHandler
+    {
+        private Mozu.Api.IApiContext _apiContext;
+
+        public async Task<IEnumerable<Mozu.Api.Contracts.ProductAdmin.Attribute>> GetAttributes(int tenantId, int? siteId,
+            int? masterCatalogId, int? startIndex, int? pageSize, string sortBy = null, string filter = null)
+        {
+            _apiContext = new ApiContext(tenantId, siteId, masterCatalogId);
+
+            var attributeResource = new AttributeResource(_apiContext);
+            var attributes = await attributeResource.GetAttributesAsync(startIndex, pageSize, sortBy, filter, null);
+
+            return attributes.Items;
+        }
+
+        public async Task<Mozu.Api.Contracts.ProductAdmin.Attribute> AddAttribute(int tenantId, int? siteId,
+            int? masterCatalogId, Mozu.Api.Contracts.ProductAdmin.Attribute attribute)
+        {
+            _apiContext = new ApiContext(tenantId, siteId, masterCatalogId);
+
+            var attributeResource = new AttributeResource(_apiContext);
+            var newAttribute = await attributeResource.AddAttributeAsync(attribute, null);
+
+            return newAttribute;
+        }
+
+        public async Task<Mozu.Api.Contracts.ProductAdmin.Attribute> UpdateAttribute(int tenantId, int? siteId,
+            int? masterCatalogId, Mozu.Api.Contracts.ProductAdmin.Attribute attribute)
+        {
+            _apiContext = new ApiContext(tenantId, siteId, masterCatalogId);
+
+            var attributeResource = new AttributeResource(_apiContext);
+            var updatedAttribute = await attributeResource.UpdateAttributeAsync(attribute, attribute.AttributeFQN, null);
+
+            return updatedAttribute;
+        }
+    }
+}
