6221e6b191fe2b32a92c2066083b44698f4b754d
mrsergio19 <sergiojtorres@outlook.com>
2015-03-19 03:37:47 -0500

    commit

---
+++ MozuDataConnector.Domain/Handlers/ProductHandler.cs
@@ -0,0 +1,49 @@
+﻿using Mozu.Api;
+using Mozu.Api.Resources.Commerce.Catalog.Admin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MozuDataConnector.Domain.Handlers
+{
+    public class ProductHandler
+    {
+        private Mozu.Api.IApiContext _apiContext;
+    
+        public async Task<IEnumerable<Mozu.Api.Contracts.ProductAdmin.Product>> GetProducts(int tenantId, int? siteId,
+            int? masterCatalogId, int? startIndex, int? pageSize, string sortBy = null, string filter = null)
+        {
+            _apiContext = new ApiContext(tenantId, siteId, masterCatalogId);
+
+            var productResource = new ProductResource(_apiContext);
+            var products = await productResource.GetProductsAsync(startIndex, pageSize, sortBy, filter, null);
+
+            return products.Items;
+        }
+
+        public async Task<Mozu.Api.Contracts.ProductAdmin.Product> AddProduct(int tenantId, int? siteId,
+            int? masterCatalogId, Mozu.Api.Contracts.ProductAdmin.Product product)
+        {
+            _apiContext = new ApiContext(tenantId, siteId, masterCatalogId);
+
+            var productResource = new ProductResource(_apiContext);
+            var newProduct = await productResource.AddProductAsync(product, null);
+
+            return newProduct;
+        }
+
+        public async Task<Mozu.Api.Contracts.ProductAdmin.Product> UpdateProduct(int tenantId, int? siteId,
+            int? masterCatalogId, Mozu.Api.Contracts.ProductAdmin.Product product)
+        {
+            _apiContext = new ApiContext(tenantId, siteId, masterCatalogId);
+
+            var productResource = new ProductResource(_apiContext);
+            var updatedProduct = await productResource.UpdateProductAsync(product, 
+                product.ProductCode, null);
+
+            return updatedProduct;
+        }
+    }
+}
