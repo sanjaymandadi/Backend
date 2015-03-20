6186e8530cea97a0c3f24d0505a7253f49ef887c
mrsergio19 <sergiojtorres@outlook.com>
2015-03-20 01:00:27 -0500

    commit

--- MozuDataConnector.Domain/Handlers/CustomerHandler.cs
+++ MozuDataConnector.Domain/Handlers/CustomerHandler.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Mozu.Api.Contracts.Customer.Credit;
 
 namespace MozuDataConnector.Domain.Handlers
 {
@@ -105,6 +106,64 @@ namespace MozuDataConnector.Domain.Handlers
                 contact.AccountId, contact.Id);
 
             return updatedcontact;
+        }
+
+        public async Task<Credit> GetCustomerCredit(int tenantId, int? siteId,
+    int? masterCatalogId, string code)
+        {
+            _apiContext = new ApiContext(tenantId, siteId, masterCatalogId);
+
+            var customerCreditResource = new CreditResource(_apiContext);
+            var credit = await customerCreditResource.GetCreditAsync(code);
+
+            return credit;
+        }
+
+        public async Task<IEnumerable<Credit>> GetCustomerCredits(int accountId, int tenantId,
+            int? siteId, int? masterCatalogId, int? startIndex, int? pageSize, string sortBy = null, string filter = null)
+        {
+            _apiContext = new ApiContext(tenantId, siteId, masterCatalogId);
+
+            var customerCreditResource = new CreditResource(_apiContext);
+            var credits = await customerCreditResource.GetCreditsAsync(startIndex, pageSize, 
+                sortBy, filter, null);
+
+            return credits.Items;
+        }
+
+        public async Task<Credit> AddCustomerCredit(Credit credit, int tenantId, 
+            int? siteId, int? masterCatalogId)
+        {
+            _apiContext = new ApiContext(tenantId, siteId);
+
+            var customerCreditResource = new CreditResource(_apiContext);
+            var newCustomerCredit = await customerCreditResource.AddCreditAsync(credit);
+
+            return newCustomerCredit;
+        }
+
+        public async Task<Credit> UpdateCustomerCredit(int tenantId, int? siteId,
+            int? masterCatalogId, Credit credit)
+        {
+            _apiContext = new ApiContext(tenantId, siteId, masterCatalogId);
+
+            var customerCreditResource = new CreditResource(_apiContext);
+            var updatedCustomerCredit = await customerCreditResource.UpdateCreditAsync(credit, credit.Code);
+
+            return updatedCustomerCredit;
+        }
+
+        public async Task<IEnumerable<CustomerSegment>> GetAccountSegments(int accountId, int tenantId, int? siteId,
+            int? masterCatalogId, int? startIndex, int? pageSize, string sortBy = null, string filter = null)
+        {
+            _apiContext = new ApiContext(tenantId, siteId, masterCatalogId);
+
+            var customerSegmentResource = new Mozu.Api.Resources.Commerce.Customer.
+                Accounts.CustomerSegmentResource(_apiContext);
+            
+            var segments = await customerSegmentResource.GetAccountSegmentsAsync(accountId, startIndex, pageSize, sortBy, filter, null);
+
+            return segments.Items;
         }
     }
 }
