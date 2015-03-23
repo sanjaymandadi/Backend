4fb0374d2ace3b14fe7d2c0558a37d0757d94a22
thomsumit <Ivana@140911>
2015-03-23 02:18:29 +0000

    Latest

--- MouDataConnector.Web/App_Start/WebApiConfig.cs
+++ MouDataConnector.Web/App_Start/WebApiConfig.cs
@@ -11,11 +11,8 @@ namespace MozuDataConnector.Web
             // Web API routes
             config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{tenantId}", new { tenantId = RouteParameter.Optional, action = "Default" }, new { id = @"\d*" });
+
         }
     }
 }
