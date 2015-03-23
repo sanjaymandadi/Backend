4b8e3331d1f75ed5b41982b6662ddf667e9255dd
thomsumit <Ivana@140911>
2015-03-23 16:21:46 +0000

    Missing files

---
+++ MozuDataConnector.Domain/Bootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using Mozu.Api.Events;
+using MozuDataConnector.Domain.Handlers;
+
+namespace MozuDataConnector.Domain
+{
+    public class Bootstrapper
+    {
+        public static void Register(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterType<ApplicationEventHandler>().As<IApplicationEvents>();
+            containerBuilder.RegisterType<OrderEventHandler>().As<IOrderEvents>();
+
+
+        }
+    }
+}
